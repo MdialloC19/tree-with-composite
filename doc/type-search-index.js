f9bbd0efcbbf04ab4f3091d6c818d3fba211dc07
Daniel André Sogo DIEME <danielandresogodieme@gmail.com>
2023-07-09 19:28:53 +0000

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"directory","l":"Directory"},{"p":"directory","l":"DirectoryComposite"},{"p":"directory","l":"File"}];updateSearchResults();
